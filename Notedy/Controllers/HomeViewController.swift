2e9ead6bffb658f65679b4214267577767dbbf95
NuntapatHi <nuntapath@gmail.com>
2022-07-29 15:21:35 +0700

    Add title's animation in homeViewController

---
+++ Notedy/Controllers/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Notedy
+//
+//  Created by Nuntapat Hirunnattee on 29/7/2565 BE.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    let appName = "Notedy"
+    @IBOutlet weak var titleName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleName.text = ""
+        var index = 0.0
+        for letter in appName{
+            Timer.scheduledTimer(withTimeInterval: 0.2 * index, repeats: false) { (timer) in
+                self.titleName.text?.append(letter)
+            }
+            index += 1
+        }
+    }
+}
+
